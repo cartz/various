533fae2706fc34ffa437053b761f4e84c7a2c374
Claus Artz <claus@mydarktime.de>
2013-03-08 17:15:02 +0100

    qr-code
    i added a new file, which contained a short descripted code of how to
    add a qr-code to a blogpost or generate it for a static url. used under
    localhost, it will display a qr-code for "mydarktime.de" in size
    300*300 px.

---
+++ qr-code.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>qr-code</title>
+	</head>
+	
+	<body>
+		<img src="http://api.qrserver.com/v1/create-qr-code/?size=300x300&data=http://mydarktime.de" alt="mydarktime.de QR Code"/>
+		</img>
+		<!--this image will display a qr code for mydarktime.de-->
+
+		<!--<img src="http://api.qrserver.com/v1/create-qr-code/?size=300x300&data="<?php the_permalink(); ?>" alt="QR Code for <?php the_title(); ?>"/></img>
+		
+		this image will display a qr code for a blogpost or something like that (wordpress) -->
+
+	</body>
+</html>
